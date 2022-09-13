6dcd8f732e40168dd55c4e35446615d61d5262aa
warshadi <warshadi@gmail.com>
2022-09-13 20:23:22 +0200

    add navbar and footer

---
+++ src/components/Footer/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import './Footer.css'
+
+export default function Footer() {
+  return (
+    <div>
+        <div class="foo">
+            <p>Copy&copy;Right For Halyna & Shadi 2022.</p>
+        </div>
+    </div>
+  )
+}
---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,32 @@
+import React from 'react'
+import './Navbar.css';
+
+function Navbar() {
+  return (
+    <div>
+        <nav className="navbar navbar-expand-lg bg-dark">
+            <div className="container">
+            <a className="navbar-brand" href="/">MMC</a>
+            <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                <span className="navbar-toggler-icon"></span>
+            </button>
+            <div className="collapse navbar-collapse" id="navbarNav">
+                <ul className="navbar-nav">
+                <li className="nav-item">
+                    <a className="nav-link" aria-current="page" href="/">Home</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="../login">Login</a>
+                </li>
+                <li className="nav-item">
+                    <a className="nav-link" href="../signup">SignUp</a>
+                </li>
+                </ul>
+            </div>
+            </div>
+        </nav>
+    </div>
+  )
+}
+
+export default Navbar;
